a493137e7b2922bb081e03e616f9ada927fea640
Andrey Kletsko <andrey.daraschenka@gmail.com>
2023-03-24 15:48:09 +0200

    Solution reverse linked list

---
+++ reverse-linked-list.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+
+        return new_head
+
+
+if __name__ == '__main__':
+    assert Solution().reverseList(ListNode(1, ListNode(2))).val == 2
