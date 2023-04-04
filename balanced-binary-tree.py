a023a270f415466a59d2167da02446dfc95b6ec1
Andrey Kletsko <andrey.daraschenka@gmail.com>
2023-04-04 14:16:19 +0300

    Update balanced-binary-tree

--- balanced-binary-tree.py
+++ balanced-binary-tree.py
@@ -1,3 +1,13 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def isSubtreeBalanced(node):
@@ -19,3 +29,7 @@ class Solution:
             )
 
         return isSubtreeBalanced(root)[0] if root else True
+
+
+if __name__ == '__main__':
+    assert Solution().isBalanced(None)
